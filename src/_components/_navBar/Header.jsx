78d0d9880a0dea029c2c4640aa3b3668d252650a
Benxxy <benjamin.dedic@hotmail.com>
2019-08-16 15:31:27 +0200

    Added links in header and logo

--- src/_components/_navBar/Header.jsx
+++ src/_components/_navBar/Header.jsx
@@ -1,51 +1,57 @@
 import React, { Component } from "react";
+import Logo from "../../images/logo.png";
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
+
+import WebSite from "../_webSite/WebSite";
 
 export default class Header extends Component {
   render() {
     return (
       <React.Fragment>
-        <nav className="navbar navbar-inverse">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <button
-                type="button"
-                className="navbar-toggle"
-                data-toggle="collapse"
-                data-target="#myNavbar"
-              >
-                <span className="icon-bar" />
-                <span className="icon-bar" />
-                <span className="icon-bar" />
-              </button>
-              <a className="navbar-brand" href="#">
-                Logo
-              </a>
+        <Router>
+          <nav className="navbar navbar-inverse">
+            <div className="container-fluid">
+              <div className="navbar-header">
+                <button
+                  type="button"
+                  className="navbar-toggle"
+                  data-toggle="collapse"
+                  data-target="#myNavbar"
+                >
+                  <span className="icon-bar" />
+                  <span className="icon-bar" />
+                  <span className="icon-bar" />
+                </button>
+                <img className="navbar-brand" src={Logo} alt="webiste logo" />
+              </div>
+              <div className="collapse navbar-collapse" id="myNavbar">
+                <ul className="nav navbar-nav">
+                  <li className="active">
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <Link ŧø="/Website">Web Site</Link>
+                  </li>
+                  <li>
+                    <Link to="/tasks">Tasks</Link>
+                  </li>
+                  <li>
+                    <Link to="/projects">Projects</Link>
+                  </li>
+                </ul>
+                <ul className="nav navbar-nav navbar-right">
+                  <li>
+                    <a href="#">
+                      <span className="glyphicon glyphicon-log-in" /> Login
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="collapse navbar-collapse" id="myNavbar">
-              <ul className="nav navbar-nav">
-                <li className="active">
-                  <a href="#">Home</a>
-                </li>
-                <li>
-                  <a href="#">About</a>
-                </li>
-                <li>
-                  <a href="#">Projects</a>
-                </li>
-                <li>
-                  <a href="#">Contact</a>
-                </li>
-              </ul>
-              <ul className="nav navbar-nav navbar-right">
-                <li>
-                  <a href="#">
-                    <span className="glyphicon glyphicon-log-in" /> Login
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+          </nav>
+
+          <Route path="/website" component={WebSite} />
+        </Router>
       </React.Fragment>
     );
   }
---
+++ src/_components/_webSite/WebSite.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class WebSite extends Component {
+  render() {
+    return <div />;
+  }
+}
