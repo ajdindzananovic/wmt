ca1e646110c6320817d9bd6857dfcb51e02fe420
Benxxy <benjamin.dedic@hotmail.com>
2019-08-20 15:48:21 +0200

    Deleted all

--- src/_components/_webSite/DomainInfo.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class DomainInfo extends Component {
-  render() {
-    return <div className="col-sm-8 text-left">Welcome in Domain Info</div>;
-  }
-}
