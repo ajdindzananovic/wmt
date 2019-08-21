45e7986c33ee2a47b0eff5fd9d7719b8db5ce47f
Ajdin Dzananovic <ajdin.dzananovic@preventgroup.com>
2019-08-21 09:21:59 +0200

    Update - form pattern  and loginForm

---
+++ src/_components/common/input.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Input = ({ onChange, name, label, value, type, error }) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <input
+        onChange={onChange}
+        value={value}
+        name={name}
+        type={type}
+        className="form-control"
+        id={name}
+      />
+      {error && <div className="alert alert-danger">{error}</div>}
+    </div>
+  );
+};
+
+export default Input;
