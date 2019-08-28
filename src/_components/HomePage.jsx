7f7e0ccf9172c38adb023d2a10805c241c739dfc
ajdindzananovic <ajdinefsa@gmail.com>
2019-08-28 14:53:17 +0200

    fix home page

--- src/_components/HomePage.jsx
+++ src/_components/HomePage.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 export default class HomePage extends Component {
   render() {
-    return <div>Welcome To Home Page of this project</div>;
+    return  <div className="container-fluid text-center">
+    <div className="row content">Welcome To Home Page of this project</div></div>;
   }
 }
